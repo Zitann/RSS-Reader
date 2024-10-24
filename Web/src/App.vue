c9598205c5889a6d7aa2fc3a5d78fb0d86abd0f1
Zitann <zitann@qq.com>
2024-10-24 23:33:41 +0800

    init

---
+++ Web/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+  <p class=" text-lg text-yellow-400 border-red-500 border-4">12318927398127389127</p>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
