3331d4868276c85bd5cd5a111a3855f5ca9d7e07
Violet-Uoo <321770977@qq.com>
2024-10-26 19:31:07 +0800

    feat(Server): 定时获取文章
    根据数据库中的feed定时爬取内容，并级联更新到数据库

--- Sever/app.ts
+++ Sever/app.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import { expressjwt } from "express-jwt";
 
-import { prisma } from "./lib/prisma";
-
 import userRouter from "./routes/user";
 import feedRouter from "./routes/feed";
+import Parser from "rss-parser";
+import job from "./utils/parse";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,19 +16,27 @@ app.use(
     secret: "secret",
     algorithms: ["HS256"],
   }).unless({
-    path: ["/login", "/register"],
+    path: ["/login", "/register", "/parser"],
   }),
 );
 
 app.use("/", userRouter);
 app.use("/feed", feedRouter);
 
-app.get("/", async (req, res) => {
-  const count = await prisma.user.count();
-  console.log(count);
-  res.send("Hello, TypeScript and Express!");
+job.invoke();
+
+const parser = new Parser();
+
+app.get("/parser", async (req, res) => {
+  const feed = await parser.parseURL("https://sspai.com/feed");
+  console.log(feed.title);
+  res.send(feed.items[0].contentSnippet);
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
+});
+
+app.on("close", () => {
+  job.cancel();
 });
---
+++ Sever/utils/parse.ts
@@ -0,0 +1,54 @@
+import Parser from "rss-parser";
+import { prisma } from "../lib/prisma";
+import schedule from "node-schedule";
+
+const parser = new Parser();
+
+export const updateArticle = async () => {
+  console.log("updateArticle");
+  const subscrptionList = await prisma.userSubscription.findMany({
+    include: {
+      feed: true,
+    },
+  });
+  for (const subscription of subscrptionList) {
+    const {
+      feed: { url, id },
+      user_id,
+    } = subscription;
+    const result = await parser.parseURL(url);
+    for (const item of result.items) {
+      const { title, link, pubDate, contentSnippet, content } = item;
+      const article = await prisma.article.findFirst({
+        where: {
+          link,
+        },
+      });
+      if (!article) {
+        await prisma.article.create({
+          data: {
+            title: title ?? "",
+            link: link ?? "",
+            content: content,
+            description: contentSnippet,
+            published_at: pubDate ? new Date(pubDate) : new Date(),
+            feed_id: id,
+            statuses: {
+              create: {
+                user_id: user_id,
+                updated_at: new Date(),
+              },
+            },
+          },
+        });
+      } else {
+        break;
+      }
+    }
+  }
+};
+
+// 每小时更新一次文章
+const job = schedule.scheduleJob("0 * * * *", updateArticle);
+
+export default job;
