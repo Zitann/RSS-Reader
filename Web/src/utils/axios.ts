d81ac48f0ba4c1caf611aa8f2fec3da3904bb394
Zitann <zitann@qq.com>
2024-11-04 23:47:51 +0800

    feat: token过期处理

--- Web/src/utils/axios.ts
+++ Web/src/utils/axios.ts
@@ -1,6 +1,7 @@
 import { baseUrl } from "./env";
 import axios from "axios";
 import tokenStore from "./store";
+import { ElNotification } from "element-plus";
 const service = axios.create({
     baseURL: baseUrl,
 })
@@ -20,11 +21,20 @@ service.interceptors.request.use(
     }
 )
 
-// // 向应拦截器
-// service.interceptors.response.use(
-//     error => {
-//         console.log(error)
-//         return error
-//     }
-// )
+// 向应拦截器
+service.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response && error.response.status === 401) {
+            ElNotification.error({
+                title: "错误",
+                message: "未授权，请重新登录"
+            })
+            const token = tokenStore()
+            token.token = ""
+            window.location.href = "/login"
+        }
+        return Promise.reject(error)
+    }
+)
 export default service
